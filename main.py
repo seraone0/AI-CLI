32dd7648687048b07e9d62ce0195316965bec0bc
KorotchenkoIgor <ogiflos@gmail.com>
2026-06-24 22:51:22 +0300

    infinite input loop; error handling

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import subprocess
 import sys
+from importlib.metadata import requires
 
 os = sys.platform
-command = "dir" if os == "win32" else "ls"
+# command = "dir" if os == "win32" else "ls"
 
-result = subprocess.run(command, shell=True, text=True, capture_output=True)
+print("Enter command: ")
 
-print('return code')
-print(result.returncode)
-print('standard output')
-print(result.stdout)
+while True:
+    request = input()
+
+    if request.lower() in ['exit', 'quit', 'выход']:
+        break
+    if not request.strip():
+        continue
+
+    if "test" in request.lower():
+        command = "mkdir test" if os == "win32" else "cd test"
+        print(f"complete {command}")
+
+        result = subprocess.run(command, shell=True, text=True, capture_output=True)
+
+        if result.returncode == 0: print('folder created')
+        else: print(f'folder creation failed {result.stderr}')
+
+    else: print(f"check '{request}', but do not connect OpenAI API, that generation info.\n")
+
